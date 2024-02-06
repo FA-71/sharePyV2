d0151e9bec26303b842ced27eb2d6cc1010b380e
oshada22 <oshada1188@gmail.com>
2024-02-06 19:35:29 +0530

    client added to program: test 1

--- program.py
+++ program.py
@@ -1,27 +1,53 @@
 import socket
+import threading
+
+
+BROADCAST_PORT = 42068
 
 
 class Server: 
     def __init__ (self): 
         return 
     
-    def start_server():
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind(('127.0.0.1', 55555))
+    def start_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server: 
+            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server.bind((socket.gethostbyname(socket.gethostname()), BROADCAST_PORT))
 
-        print("Server started. Waiting for devices...")
+            print("Server started. Waiting for devices...")
 
-        while True:
-            data, addr = server_socket.recvfrom(1024)
-            print(f"Received message from {addr}: {data.decode()}")
-            # Optionally, you can add logic to verify if the received message is from a valid device and perform actions accordingly
+            while True:
+                data, addr = server.recvfrom(1024)
+                print(f"Received message from {addr}: {data.decode()}")
 
 
+class Client: 
+    def __init__(self): 
+        return 
+    
+    def broadcast_self(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+            broadcast_addr = ('<broadcast>', BROADCAST_PORT)
+            print("sending ")
+            for i in range(5): 
+                s.sendto("this is test".encode(), broadcast_addr)
+        
 
 def main():
     server = Server()
-    server.start_server()
+    server_thread = threading.Thread(target=server.start_server)
+    server_thread.start()
+
+    if (input("enter y: ") == "y"): 
+        client = Client() 
+        client_thread = threading.Thread(target=client.broadcast_self)
+        client_thread.start()
+
+    server_thread.join()
+    client_thread.join()
 
 
 if __name__ == "__main__":
